c21d709ab029c7ae093937bae2dea619f0fd2416
Alex <alex0692pash@mail.ru>
2021-09-13 21:47:47 +0300

    002 init

---
+++ 002_Task2/cmake-build-debug/CMakeFiles/002_Task2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "002_Task2.exe"
+  "002_Task2.exe.manifest"
+  "002_Task2.pdb"
+  "CMakeFiles/002_Task2.dir/main.cpp.obj"
+  "lib002_Task2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/002_Task2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
