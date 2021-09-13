e452758a88562ebe40b53b74ba453c1988b351bb
Alex <alex0692pash@mail.ru>
2021-09-13 21:41:02 +0300

    001 init

---
+++ 001_Task1/cmake-build-debug/CMakeFiles/001_Task1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "001_Task1.exe"
+  "001_Task1.exe.manifest"
+  "001_Task1.pdb"
+  "CMakeFiles/001_Task1.dir/main.cpp.obj"
+  "lib001_Task1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/001_Task1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
