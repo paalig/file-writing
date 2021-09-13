be3b3882d14d337c924884438447fe1007fe2ff9
Alex <alex0692pash@mail.ru>
2021-09-13 21:54:06 +0300

    004 init

---
+++ 004_Task4/cmake-build-debug/CMakeFiles/004_Task4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "004_Task4.exe"
+  "004_Task4.exe.manifest"
+  "004_Task4.pdb"
+  "CMakeFiles/004_Task4.dir/main.cpp.obj"
+  "lib004_Task4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/004_Task4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 004_Task4/cmake-build-debug/CMakeFiles/004_Task4.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 004_Task4.
+# This may be replaced when dependencies are built.
