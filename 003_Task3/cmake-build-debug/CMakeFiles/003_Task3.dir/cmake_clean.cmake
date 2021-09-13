74eeb1597bdc91cd782a3a6abd91fdff3438424c
Alex <alex0692pash@mail.ru>
2021-09-13 21:51:12 +0300

    003 init

---
+++ 003_Task3/cmake-build-debug/CMakeFiles/003_Task3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "003_Task3.exe"
+  "003_Task3.exe.manifest"
+  "003_Task3.pdb"
+  "CMakeFiles/003_Task3.dir/main.cpp.obj"
+  "lib003_Task3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/003_Task3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
